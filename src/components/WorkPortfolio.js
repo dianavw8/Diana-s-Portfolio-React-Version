124b7e9bee09ca2f7a88e68ba3637002614becdf
Diana <dianavw8@gmail.com>
2023-04-04 23:45:36 -0400

    added ScoreSocial & additonal row of projects in workportfolio

--- src/components/WorkPortfolio.js
+++ src/components/WorkPortfolio.js
@@ -6,24 +6,15 @@ function WorkPortfolio() {
         <h1 style={{marginTop : "65px"}} className="text-center project-item">PORTFOLIO </h1>
         <p className="portfolio-sub-description">Below are some examples of my work. Stay tuned for more! </p>
         <hr className="divider-line"/>
-        {/* <div className="row">
-          <div className="col-md-12">
-            <div className="project-item main-project-item">
-              <a href="https://radiant-tor-97650.herokuapp.com/">
-                <img src={`${process.env.PUBLIC_URL}/assets/Tech_Blog_Comments_.jpg`} alt="Tech Blog application"/>
-                <p>Tech Blog</p>
-              </a>
-            </div>
-          </div>
-        </div> */}
+          
         <div className="row">
           <div className="col-md-4">
             <div className="project-item">
-              <a href="https://project-2-spotter.herokuapp.com/">
-                <img src={`${process.env.PUBLIC_URL}/assets/Spotter_homepage.jpg`} alt="Spotter application"/>
-                <p>Spotter</p>
+              <a href="https://mighty-cliffs-23244.herokuapp.com/">
+                <img src={`${process.env.PUBLIC_URL}/assets/SSlanding.jpg`} alt="ScoreSocial application"/>
+                <p>ScoreSocial</p>
               </a>
-              <a href="https://github.com/mhengerer/Spotter" target="_blank" rel="noopener noreferrer">GitHub Repo</a>
+              <a href="https://github.com/dianavw8/ScoreSocial" target="_blank" rel="noopener noreferrer">GitHub Repo</a>
             </div>
           </div>
           <div className="col-md-4">
@@ -56,13 +47,13 @@ function WorkPortfolio() {
                 <a href="https://github.com/dianavw8/Timed-Coding-Quiz" target="_blank" rel="noopener noreferrer">GitHub Repo</a>
               </div>
             </div>
-        <div className="col-md-4">
+            <div className="col-md-4">
             <div className="project-item">
-              <a href="https://beefbones.github.io/travel-planner/">
-                <img src={`${process.env.PUBLIC_URL}/assets/Travel-Planner-Group-Project1.jpg`} alt="Image of Travel Planner webpage"/>
-                <p>Travel Planner</p>
+              <a href="https://project-2-spotter.herokuapp.com/">
+                <img src={`${process.env.PUBLIC_URL}/assets/Spotter_homepage.jpg`} alt="Spotter application"/>
+                <p>Spotter</p>
               </a>
-              <a href="https://github.com/beefbones/travel-planner" target="_blank" rel="noopener noreferrer">GitHub Repo</a>
+              <a href="https://github.com/mhengerer/Spotter" target="_blank" rel="noopener noreferrer">GitHub Repo</a>
             </div>
           </div>
           <div className="col-md-4">
@@ -74,36 +65,48 @@ function WorkPortfolio() {
               <a href="https://github.com/dianavw8/Work-Day-Scheduler" target="_blank" rel="noopener noreferrer">GitHub Repo</a>
             </div>
           </div>
-          {/* <div className="col-md-4">
+        <div className="row">
+          <div className="col-md-4">
             <div className="project-item">
               <a href="https://dianavw8.github.io/Horiseon_Accessability-_Challenge/">
                 <img src={`${process.env.PUBLIC_URL}/assets/HorisonPhoto.jpg`} alt="Image of Horiseon webpage"/>
-                <p>Horiseon Accessability Challenge</p>
+                <p>Horiseon Accessibility Challenge</p>
               </a>
-              <a href="" target="_blank" rel="noopener noreferrer">GitHub Repo</a>
+              <a href="https://dianavw8.github.io/Horiseon_Accessability-_Challenge/" target="_blank" rel="noopener noreferrer">GitHub Repo</a>
             </div>
           </div>
           <div className="col-md-4">
             <div className="project-item">
-              <a href="#">
-                <img src={"https://t3.ftcdn.net/jpg/02/87/36/18/360_F_287361836_9hMr3LwNXBu1zdJnifR5X2A8N4CNCxBC.jpg"} alt="Coming Soon image"/>
-                <p>Future Project</p>
+              <a href="https://beefbones.github.io/travel-planner/">
+                <img src={`${process.env.PUBLIC_URL}/assets/Travel-Planner-Group-Project1.jpg`} alt="Image of Travel Planner webpage"/>
+                <p>Travel Planner</p>
               </a>
-              <a href="" target="_blank" rel="noopener noreferrer">GitHub Repo</a>
+              <a href="https://github.com/beefbones/travel-planner" target="_blank" rel="noopener noreferrer">GitHub Repo</a>
             </div>
           </div>
           <div className="col-md-4">
             <div className="project-item">
-              <a href="#">
-                <img src={"https://t3.ftcdn.net/jpg/02/87/36/18/360_F_287361836_9hMr3LwNXBu1zdJnifR5X2A8N4CNCxBC.jpg"} alt="Coming Soon image"/>
-                <p>Future Project</p>
+              <a href="https://radiant-tor-97650.herokuapp.com/">
+                <img src={`${process.env.PUBLIC_URL}/assets/Tech_Blog_Comments_.jpg`} alt="Tech Blog application"/>
+                <p>Tech Blog</p>
               </a>
-              <a href="" target="_blank" rel="noopener noreferrer">GitHub Repo</a>
-            </div> */}
+              <a href="https://github.com/dianavw8/Tech_Blog" target="_blank" rel="noopener noreferrer">GitHub Repo</a>
+            </div>
           </div>
         </div>
+          {/* <div className="col-md-4">
+            <div className="project-item">
+              <a href="#">
+                <img src={"https://t3.ftcdn.net/jpg/02/87/36/18/360_F_287361836_9hMr3LwNXBu1zdJnifR5X2A8N4CNCxBC.jpg"} alt="Coming Soon image"/>
+                <p>Future Project</p>
+              </a>
+              <a href="https://github.com/dianavw8" target="_blank" rel="noopener noreferrer">GitHub Repo</a>
+            </div>
+          </div> */}
+        </div>
       </div>
+    </div>
     );
-  }
+}
   
   export default WorkPortfolio;
